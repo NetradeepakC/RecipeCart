ca9b00c415c0f8c790045af83d9d296847359547
Netradeepak Chinchwadkar <netradeepakc_al@localhost.localdomain>
2023-02-03 19:10:05 +0530

    Initialised Project

---
+++ src/main/kotlin/com/netradeepak/recipecart/RecipecartApplication.kt
@@ -0,0 +1,11 @@
+package com.netradeepak.recipecart
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RecipecartApplication
+
+fun main(args: Array<String>) {
+	runApplication<RecipecartApplication>(*args)
+}
---
+++ src/test/kotlin/com/netradeepak/recipecart/RecipecartApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.netradeepak.recipecart
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RecipecartApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
